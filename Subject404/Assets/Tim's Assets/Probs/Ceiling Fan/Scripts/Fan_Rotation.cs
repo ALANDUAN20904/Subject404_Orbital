f7a5dfad0e2d0c62887c9749faa934f867290fd8
Duan Yihe <dtc20904newgame@gmail.com>
2024-05-20 11:20:51 +0800

    imported fan asset closes #3,enhances door animation based on #6

---
+++ Subject404/Assets/Tim's Assets/Probs/Ceiling Fan/Scripts/Fan_Rotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Fan_Rotation : MonoBehaviour {
+void Start()
+	{
+
+	}
+
+
+	
+void Update () {
+		
+		//rotation around y, changable speed, spinned normalised based on rendering speed
+
+		transform.Rotate(0f,100*Time.deltaTime,0f, Space.Self); 
+	}
+}
