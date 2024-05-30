aa30cdb29fbd15c50303b48d05d870c2fae8ebef
unknown <nivedit@comp.nus.edu.sg>
2024-05-30 17:58:26 +0800

    set up game instruction text sequence based on task completion #21

--- Subject404/Assets/StateManager.cs
+++ Subject404/Assets/StateManager.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 public class StateManager : MonoBehaviour
 {
     private TextUpdater textUpdater;
-    public int sceneState = 0;
-    string[] instructions = {"Grab lamp interactable to turn on lamp", "Walk towards the table"};
+    private int sceneState = 0;
+    public GameObject lampActive;
+    private bool playedAudio = false;
+    private bool interactedFridge = false;
+    public void setInteractedFridge(){
+        interactedFridge = true;
+    }
+    public void setPlayedAudio(){
+        playedAudio = true;
+    }
+
+    string[] instructions = {"Grab lamp interactable to turn on lamp", "Walk towards the table", "Open the fridge", "Exit the house"};
     private void Awake(){
         textUpdater = GetComponent<TextUpdater>();
     }
     private void Update(){
+        if (!lampActive.activeSelf) sceneState = 0;
+        else if (!playedAudio) sceneState = 1;
+        else if (!interactedFridge) sceneState = 2;
+        else sceneState = 3;
+
         string text = instructions[sceneState];
         textUpdater.UpdateText(ref text);
     }
---
+++ Subject404/Assets/TriggerTable.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTable : MonoBehaviour
+{
+    private StateManager stateManager;
+    public GameObject gameInstructions;
+    private void Awake(){
+        stateManager = gameInstructions.GetComponent<StateManager>();
+    }
+    private void OnTriggerEnter(Collider other){
+        Debug.Log("Entered");
+        stateManager.setPlayedAudio();
+    }
+}
