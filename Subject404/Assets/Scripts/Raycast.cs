3333e96c6052ba9a5a66ec855adb48c3463ac3b4
unknown <nivedit@comp.nus.edu.sg>
2024-06-21 11:49:35 +0530

    add error handling for edge cases in Raycast script #89

--- Subject404/Assets/Scripts/Raycast.cs
+++ Subject404/Assets/Scripts/Raycast.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class Raycast : MonoBehaviour
 {
-    Ray ray;
-    RaycastHit raycastHit;
     public LayerMask layerMask;
 
     public bool IsObjectInView(GameObject target)
     {
-        ray = new Ray(transform.position, transform.forward);
-        Vector3 vec = target.transform.position - transform.position;
-        if (Physics.Raycast(ray, out raycastHit, layerMask)){
-            Vector3 vec2 = raycastHit.point - transform.position;
-            return Vector3.Angle(vec2, vec) < 45.0f;
+        if (target != null){
+            Ray ray = new Ray(transform.position, transform.forward);
+            Vector3 vec = target.transform.position - transform.position;
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray, out raycastHit, layerMask)){
+                Vector3 vec2 = raycastHit.point - transform.position;
+                return !hasObstacle(target) && Vector3.Angle(vec2, vec) < 45.0f;
+            }
+            return false;
         }
+        Debug.LogError("Raycast target not provided");
         return false;
+    }
+
+    private bool hasObstacle(GameObject target)
+    {
+        Ray obstacleCheckRay = new Ray(transform.position, target.transform.position - transform.forward);
+        RaycastHit obstacleHit;
+        if (Physics.Raycast(obstacleCheckRay, out obstacleHit, layerMask)){
+            return obstacleHit.collider.gameObject.name != target.name;
+        }
+        return true;
     }
 }
