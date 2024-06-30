acf6754592a4ba5e44258fdd6574edc3f13ce535
Duan Yihe <dtc20904newgame@gmail.com>
2024-07-01 02:44:28 +0900

    added policeman asset and scene3 game instructions, npc interactions, state manager #53 #48 #24 #52

---
+++ Subject404/Assets/DialoguePoliceman.cs
@@ -0,0 +1,53 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialoguePoliceman : MonoBehaviour
+{
+    private int _sceneState;
+    private bool[] _flag;
+    public AudioSource audioSource;
+    public GameObject gameInstructions;
+    public AudioClip[] audioClips;
+    private StateManagerScene3 _stateManager;
+    void Start()
+    {
+        _stateManager = gameInstructions.GetComponent<StateManagerScene3>();
+        if (_stateManager == null)
+        {
+            Debug.LogError("State Manager not found");
+        }
+        if (audioSource == null)
+        {
+            Debug.LogError("Audio Source not set");
+        }
+        _flag = new bool[] { true, false, true, false, true, true, true, true, true, true, true };
+    }
+
+    private IEnumerator WaitAndUpdateState()
+    {
+        yield return new WaitForSeconds(audioClips[_sceneState].length);
+        _stateManager.SetSceneState(_sceneState + 1);
+    }
+
+    void Update()
+    {
+        _sceneState = _stateManager.GetSceneState();
+        if (!_flag[_sceneState])
+        {
+            _flag[_sceneState] = true;
+            if (audioClips[_sceneState] != null)
+            {
+                audioSource.clip = audioClips[_sceneState];
+                audioSource.Play();
+                StartCoroutine(WaitAndUpdateState());
+            }
+            else
+            {
+                Debug.LogError("Audio Clip not found");
+            }
+        }
+    }
+
+}
--- Subject404/Assets/StateManagerScene3.cs
+++ Subject404/Assets/StateManagerScene3.cs
@@ -5,15 +5,77 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class StateManagerScene3 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int _sceneState = 0;
+    public GameObject DoorKnob;
+    public GameObject Milk;
+    public GameObject Policeman;
+    private DialoguePoliceman _policeDialogue;
+    private bool _triggeredAudio = false;
+    private bool _playedAudio = false;
+    private AudioSource _audioSources;
+    private ToggleInteraction _toggleInteraction;
+    private bool _grabbedMilk = false;
+    private bool _putDownMilk = false;
+    private TextUpdater _textUpdater;
+    string[] _instructions = {"Grab a milk","","","","","Put the milk down at the cashier","","","","","Grab door knob to exit" };
+
+
     void Start()
     {
-        
+        _policeDialogue = Policeman.GetComponent<DialoguePoliceman>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
         
+        _textUpdater = GetComponent<TextUpdater>();
+        if (_textUpdater == null)
+        {
+            Debug.LogError("Text Updater component not found");
+        }
+        
+        _sceneState = 0;
     }
+
+    public int GetSceneState()
+    {
+        return _sceneState;
+    }
+    public void SetSceneState(int state)
+    {
+        _sceneState = state;
+    }
+    public void SetGrabbedMilk()
+    {
+        _grabbedMilk = true;
+    }
+
+    public void SetPutDownMilk()
+    {
+        _putDownMilk = true;
+    }
+
+
+    void Update()
+    {
+        string text = _instructions[_sceneState];
+        _textUpdater.UpdateText(ref text);
+
+        if (_grabbedMilk)
+        {
+            _sceneState = 1;
+        }
+        else if (_putDownMilk)
+        {
+            _sceneState = 5;
+        }
+
+      
+    }
+
+
+
+
+
+
 }
