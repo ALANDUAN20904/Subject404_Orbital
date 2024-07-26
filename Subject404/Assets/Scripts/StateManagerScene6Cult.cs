0108f67914fc733e4d9918582c486c056f0f795c
unknown <nivedit@comp.nus.edu.sg>
2024-07-26 16:30:18 +0800

    implement scene transition

---
+++ Subject404/Assets/Scripts/StateManagerScene6Cult.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManagerScene6Cult : MonoBehaviour
+{
+    private int _sceneState = 0;
+    private TextUpdater _textUpdater;
+    private string[] _instructions = {"Walk towards the cultists and take a picture"};
+
+    public int GetSceneState()
+    {
+        return _sceneState;
+    }
+
+    public void SetSceneState(int sceneState)
+    {
+        _sceneState = sceneState;
+    }
+    void Start()
+    {
+        _textUpdater = GetComponent<TextUpdater>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string text = _instructions[_sceneState];
+        _textUpdater.UpdateText(ref text);
+    }
+}
