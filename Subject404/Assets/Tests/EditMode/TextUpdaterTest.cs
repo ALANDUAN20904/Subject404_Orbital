7e293f752195104105cb0a69410477572745438b
unknown <nivedit@comp.nus.edu.sg>
2024-08-09 20:54:15 +0800

    update tests

--- Subject404/Assets/Tests/EditMode/TextUpdaterTest.cs
+++ Subject404/Assets/Tests/EditMode/TextUpdaterTest.cs
@@ -9,6 +9,13 @@ using TMPro;
 public class TextUpdaterTest
 {
     private TMP_Text _textField;
+    public RectTransform panel;
+    public GameObject UI;
+    private Vector3 _originalScale;
+    private Vector3 targetScale = new Vector3(0.5f, 0.5f, 0.5f);
+    private float transitionDuration = 1.0f;
+    private Vector3 _originalPosition;
+    private Vector3 targetPosition = new Vector3(0, -40, 0);
     private TextUpdater _textUpdater;
     private string _text;
     private TMP_FontAsset _fontAsset;
@@ -23,6 +30,12 @@ public class TextUpdaterTest
         _fontAsset = UnityEditor.AssetDatabase.LoadAssetAtPath<TMP_FontAsset>(fontAssetPath);
         _textField = textObject.AddComponent<TextMeshProUGUI>();
         _textField.font = _fontAsset;
+        UI = new GameObject("UI");
+        _textUpdater.UI = UI;
+        panel = UI.AddComponent<RectTransform>();
+        _textUpdater.panel = panel;
+        _originalScale = panel.localScale;
+        _originalPosition = panel.localPosition;
 
         if (_fontAsset == null)
         {
